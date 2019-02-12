0b013c8f00c09c610a4c7d89ea3214b676c8a101
fly1ngDream <eugen228228@gmail.com>
2019-02-12 17:38:59 +0200

    'Admin static files and .gitattribute file were removed'

--- textFilesStorage/settings/base.py
+++ textFilesStorage/settings/base.py
@@ -52,8 +52,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
