1d30c94bd92ce69bbb6d91e5e3b0f3234e460b65
fly1ngDream <eugen228228@gmail.com>
2019-02-17 23:59:05 +0200

    Password reset via email added

--- textFilesStorage/settings/base.py
+++ textFilesStorage/settings/base.py
@@ -142,3 +142,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '***REMOVED***'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
