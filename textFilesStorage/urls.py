442cc0d36be88cdc440f3666eb7e20ec7477088a
fly1ngDream <eugen228228@gmail.com>
2019-02-18 21:45:37 +0200

    Tags links were added to files list and files details

--- textFilesStorage/urls.py
+++ textFilesStorage/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     ),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('files', include('files.urls')),
+    path('files/', include('files.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
